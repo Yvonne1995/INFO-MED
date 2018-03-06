ee293d60c4fe48f196ea9a29af7f9fe693b370fe
Wesleymutwiri <wesleymochiemo@gmail.com>
2018-03-06 15:17:35 -0500

    New js and records files

--- records.js
+++ records.js
@@ -1,1 +1,14 @@
-//business logic
+//UI logic
+$(document).ready(function() {
+  $("form#save").submit(function(event){}
+  event.preventDefault();
+  function myFunction() {
+      var records = document.getElementById("hidden");
+      if (records.style.display === "none") {
+          records.style.display = "block";
+      } else {
+          records.style.display = "none";
+      }
+  }
+
+});
